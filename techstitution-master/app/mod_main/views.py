23a5fa537895d05177d438daa7c5b5cac38bea55
bernazeri <bernazehri@gmail.com>
2020-05-25 20:24:11 +0200

    Techtstitution

---
+++ techstitution-master/app/mod_main/views.py
@@ -0,0 +1,14 @@
+from flask import Blueprint, render_template
+from app import mongo
+mod_main = Blueprint('main', __name__)
+
+@mod_main.route('/')
+def index():
+	db = mongo.db.arkep
+	db.insert({"name" : "arkep"})
+	return render_template("index.html")
+
+@mod_main.route('/form')
+def form():
+	name="Techstitution"
+	return render_template("form.html", name=name)
